ddefd5e7d8e96295bb7399a149c46708bd0d8e0a
Alex Marshall <43850844+Alex202Mar@users.noreply.github.com>
2023-07-19 15:37:19 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';   
+import List from './List';
+
+function App() {
+  return (
+    <div class="centereditem">
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ List.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+function List() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('');
+
+  const addItem = () => {
+    if (newItem.trim() !== '') {
+      setItems([...items, newItem]);
+      setNewItem('');
+    }
+  };
+
+  const removeItem = (index) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  };
+
+  return (
+    <div>
+      <h1>List of Items</h1>
+      <input
+        type="text"
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+      <button onClick={addItem}>Add Item</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => removeItem(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default List;
